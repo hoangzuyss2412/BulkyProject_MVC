1c7fe9c08870b4077904737a009586a14a38babc
hoangzuyss2412 <emcuangaymailax2003@gmail.com>
2024-03-18 18:52:12 +1000

    Create category page and add validation errors

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,5 +16,28 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();  
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index", "Category");
+			}
+
+
+            return View();            
+        }
     }
 }
