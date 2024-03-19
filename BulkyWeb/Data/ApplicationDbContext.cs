b64055595967c3366efda490390ef6f026066335
hoangzuyss2412 <emcuangaymailax2003@gmail.com>
2024-03-19 16:52:55 +1000

    Add Edit Controlller and View

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -17,8 +17,9 @@ namespace BulkyWeb.Data
             modelBuilder.Entity<Category>().HasData(
                     new Category { Id = 1, Name = "Action", DisplayOrder = 1},
                     new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
-                    new Category { Id = 3, Name = "History", DisplayOrder = 3 }
-                );
+                    new Category { Id = 3, Name = "History", DisplayOrder = 3 },
+					new Category { Id = 7, Name = "Romantic", DisplayOrder = 8 }
+				);
         }
     }
 }
