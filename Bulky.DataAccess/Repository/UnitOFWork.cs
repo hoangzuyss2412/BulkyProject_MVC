6ec13e21113c68b1a083d886d402749bf5743a6d
hoangzuyss2412 <emcuangaymailax2003@gmail.com>
2024-03-24 20:56:02 +1000

    Add UnitOfWork

--- Bulky.DataAccess/Repository/UnitOFWork.cs
+++ Bulky.DataAccess/Repository/UnitOFWork.cs
@@ -1,27 +1,27 @@
 ﻿using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
-using Bulky.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository
 {
-	public class CategoryRepository : Repository<Category>, ICategoryRepository
+	public class UnitOFWork : IUnitOfWork
 	{
 		private readonly ApplicationDbContext _db;
-        public CategoryRepository(ApplicationDbContext db) : base(db)
+		public ICategoryRepository CategoryRepository { get; private set; }
+
+		public UnitOFWork(ApplicationDbContext db) 
 		{
 			_db = db;
+			CategoryRepository = new CategoryRepository(_db);
 		}
-      
 
-		public void Update(Category obj)
+		public void Save()
 		{
-			_db.Categories.Update(obj);	
+			_db.SaveChanges();
 		}
 	}
 }
